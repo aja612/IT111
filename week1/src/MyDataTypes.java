01554b82880193fa0161e28742dd2e0f8bbb2bf1
aja612 <ndeyeadiara7@gmail.com>
2025-04-22 11:05:41 -0700

    Add files via upload

---
+++ week1/src/MyDataTypes.java
@@ -0,0 +1,21 @@
+public class MyDataTypes {
+    public static void main(String[] args) {
+
+        short number1 = 32_000;
+        int number5 = 2_000_000;
+        long number2 = 3_000_000_000L;
+        float number3 = 30.57F;
+        double number4 = 50.45637377;
+
+
+
+
+
+
+
+
+
+        boolean dry = false;
+
+    }
+}
---
+++ week1/src/MyFirstFile.java
@@ -0,0 +1,6 @@
+public class MyFirstFile {
+
+    public static void main(String[] args){
+        System.out.println("my first file");
+    }
+}
